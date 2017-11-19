f7490c297f5f69edd50fa649d7c909301dba3533
yaniv cohen <yanivomc@gmail.com>
2017-11-19 23:04:25 +0200

    test 2

--- travisci_client/travisci_client.py
+++ travisci_client/travisci_client.py
@@ -4,7 +4,7 @@ t = TravisPy.github_auth('a8a470484a3876aa338e8fddfa3cfcdcfb037850')
 user = t.user()
 repos = t.repos(member=user.login)
 
-print ("User name logged:", user.login)
+print (" User name logged:", user.login)
 
 print len(repos)
 print ("Project name ", repos[0].slug)
